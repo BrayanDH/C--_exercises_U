ba30b92a381990eb14cbcc256c694666a00dcc58
BrayanDH <bdhdavid.bh@gmail.com>
2023-07-19 08:45:43 -0600

    agregado salida al archivo:Trabajando_con_archivos

--- Lenguajes_I/Unidad_II/trabajando_con_archivos.cpp
+++ Lenguajes_I/Unidad_II/trabajando_con_archivos.cpp
@@ -32,6 +32,9 @@ int main(void)
   // Datos a grabar en el archivo
   string nombre, apellido;
 
+  // guardar los datos en un arreglo bi dimensional
+  string datos[cantidad][2];
+
   // Grabar datos en el archivo
   for (int i = 0; i < cantidad; i++)
   {
@@ -45,11 +48,24 @@ int main(void)
     cout << "digite Apellido: ";
     cin >> apellido;
 
+    // Guardar datos ingresados en el arreglo
+    datos[i][0] = nombre;
+    datos[i][1] = apellido;
+
     // Grabar datos en el archivo
     archivo << "Nombre: " << nombre << endl;
     archivo << "Apellido: " << apellido << endl;
     archivo << endl;
     system("CLS");
+  }
+
+  // Mostrar los datos guardos en el arreglo
+  cout << "Datos guardados en el arreglo: " << endl;
+  for (int i = 0; i < cantidad; i++)
+  {
+    cout << "Nombre: " << datos[i][0] << endl;
+    cout << "Apellido: " << datos[i][1] << endl;
+    cout << endl;
   }
 
   // Cerrar el archivo
