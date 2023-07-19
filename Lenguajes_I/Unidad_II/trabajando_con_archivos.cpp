06f71f084a6f51cc7f5c24c35a2980100b3cb4b9
BrayanDH <bdhdavid.bh@gmail.com>
2023-07-19 08:39:06 -0600

    agregado ejercicio con archivos

---
+++ Lenguajes_I/Unidad_II/trabajando_con_archivos.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <fstream> // Incluir la biblioteca para trabajar con archivos
+#include <string>
+#include <wchar.h>
+
+#include <stdio.h>
+#include <locale.h>
+#include <stdlib.h>
+using namespace std;
+
+int main(void)
+{
+  setlocale(LC_ALL, "");
+  // Abrir el archivo en modo de escritura con la opci�n de estar al final del archivo
+  ofstream archivo("datos.txt", std::ios::app | std::ios::ate);
+
+  // Verificar si el archivo se abri� correctamente
+  if (!archivo.is_open())
+  {
+    cerr << "Error al abrir el archivo." << endl;
+    return 0;
+  }
+
+  // Colocar el puntero al final del archivo (opcional, para asegurarse)
+  archivo.seekp(0, std::ios::end);
+
+  // Pedir la cantidad de datos que se van a grabar
+  int cantidad;
+  cout << "Ingrese la cantidad de datos que quiere ingresar: " << endl;
+  cin >> cantidad;
+
+  // Datos a grabar en el archivo
+  string nombre, apellido;
+
+  // Grabar datos en el archivo
+  for (int i = 0; i < cantidad; i++)
+  {
+
+    cout << "Grabando el archivo " << i + 1 << " de " << cantidad << endl;
+    // Pedir datos
+    nombre = "";
+    apellido = "";
+    cout << "digite nombre: ";
+    cin >> nombre;
+    cout << "digite Apellido: ";
+    cin >> apellido;
+
+    // Grabar datos en el archivo
+    archivo << "Nombre: " << nombre << endl;
+    archivo << "Apellido: " << apellido << endl;
+    archivo << endl;
+    system("CLS");
+  }
+
+  // Cerrar el archivo
+  archivo.close();
+
+  cout << "Datos grabados en el archivo correctamente." << endl;
+
+  return EXIT_SUCCESS;
+}
